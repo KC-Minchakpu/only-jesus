381b510943384e17ece26716e5a4fdcf8ebb967d
KEVIN CROSS MINCHAKPU <octatesystems@outlook.com>
2025-12-17 19:02:50 +0100

    commit

--- src/app/login/[[...rest]]/page.js
+++ src/app/login/[[...rest]]/page.js
@@ -1,12 +1,12 @@
 "use client";
 
-import { SignIn } from "@clerk/nextjs";
+
 import styles from "./login.module.css";
 
 export default function LoginPage() {
   return (
     <div className={styles.container}>
-      <SignIn routing="path" path="/login" />
+      
     </div>
   );
 }
--- src/app/register/[[...rest]]/page.js
+++ src/app/register/[[...rest]]/page.js
@@ -1,12 +1,11 @@
 "use client"; 
 
-import { SignUp } from "@clerk/nextjs";
 import styles from "./register.module.css";
 
 export default function RegisterPage() {
   return (
     <div className={styles.container}>
-      <SignUp/>
+      
     </div>
   );
 }
